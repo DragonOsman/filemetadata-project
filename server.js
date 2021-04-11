d0ff61ec4b3512b12716f5fd8269fc79508bf2d5
Osman Zakir <osmanzakir90@hotmail.com>
2021-04-11 19:04:30 +0500

    used path.join in app.use on line 9 for the path instead of concatenation and switched from single quotes to double quotes in file

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config()
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", path.join("express.static(process.cwd())", "/public"));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
