0d8f88e2ae97691fafb7089d95c42af1cd4f6415
Osman Zakir <osmanzakir90@hotmail.com>
2021-04-12 20:26:44 +0500

    installed body-parser to parse POST data, changed div with class container to main and added nested and usage example inside header; added app.use for bodyParser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config()
 const path = require("path");
+const multer = require("multer");
 
 const app = express();
 
