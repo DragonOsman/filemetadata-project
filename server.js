ca5ce80d730d652de426ddd8f3e89760ae6d135e
Osman Zakir <osmanzakir90@hotmail.com>
2021-04-12 20:40:03 +0500

    changed functions on lines 14 and 24 to use arrow syntax

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config()
+require("dotenv").config();
 const path = require("path");
 const multer = require("multer");
 const bodyParser = require("body-parser");
@@ -10,14 +10,16 @@ const app = express();
 app.use(cors());
 app.use("/public", path.join("express.static(process.cwd())", "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(`${process.cwd()}/views/index.html`);
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.post("/api/fileanalyse");
+
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log(`Your app is listening on port ${port}`)
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
 });
